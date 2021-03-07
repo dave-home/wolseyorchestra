6bb8d3b90420a17f7b0659f9eacf6acf6d228b36
dave-home <dave@corno.co.uk>
2021-03-07 09:12:40 +0000

    initial upload

---
+++ 2010/inc/script.js
@@ -0,0 +1,6 @@
+function openWin( url )
+{
+    window.open( url, 'mywindow', 'toolbar=yes,location=yes,directories=yes,status=yes,menubar=yes,scrollbars=yes,copyhistory=yes,resizable=yes' );
+}
+
+
---
+++ inc/script.js
@@ -0,0 +1,5 @@
+function openWin( url )
+{
+    window.open( url, 'mywindow', 'toolbar=yes,location=yes,directories=yes,status=yes,menubar=yes,scrollbars=yes,copyhistory=yes,resizable=yes' );
+}
+
